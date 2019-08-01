012e1857e0c516884e791a543e638599749b9db5
Harry Pierson <harrypierson@outlook.com>
2019-08-01 16:02:04 -0700

    fix launch for netcoreapp2.1 change

--- src/neo-express/launch.ps1
+++ src/neo-express/launch.ps1
@@ -1,9 +1,12 @@
 param([int]$secondsPerBlock = 1, [switch]$debug, [switch]$reset, [string]$checkpoint)
 
 dotnet publish
+
+$expressDllPath = '.\bin\Debug\netcoreapp2.1\publish\neo-express.dll'
+
 function launch($index) {
 	$resetArg = if ($reset) { "--reset" } else { "" }
-	cmd /c start dotnet .\bin\Debug\netcoreapp2.2\publish\neo-express.dll run $index --seconds-per-block $secondsPerBlock $resetArg 
+	cmd /c start dotnet $expressDllPath run $index --seconds-per-block $secondsPerBlock $resetArg 
 }
 
 if ([string]::IsNullOrEmpty($checkpoint)) {
@@ -12,5 +15,5 @@ if ([string]::IsNullOrEmpty($checkpoint)) {
 	$nodes = if ($debug) { 1..$lastNodeIndex } else { 0..$lastNodeIndex }
 	$nodes | ForEach-Object { launch $_ }
 } else {
-	cmd /c start dotnet .\bin\Debug\netcoreapp2.2\publish\neo-express.dll checkpoint run $checkpoint --seconds-per-block $secondsPerBlock
+	cmd /c start dotnet $expressDllPath checkpoint run $checkpoint --seconds-per-block $secondsPerBlock
 }

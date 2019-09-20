323b522fff3acb81ebde4f796218a3cb940dd2a7
Harry Pierson <harrypierson@outlook.com>
2019-09-20 14:43:51 -0700

    debug server PoC working

---
+++ src/neo-express/Node/DebugExecutionEngine.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol.Messages;
+using Neo.SmartContract;
+using Neo.VM;
+using NeoDebug;
+using NeoDebug.VariableContainers;
+using System.Collections.Generic;
+
+namespace NeoExpress.Node
+{
+    internal class DebugExecutionEngine : ApplicationEngine, IExecutionEngine
+    {
+        private Neo.UInt160 scriptHash;
+        private readonly Neo.Persistence.Snapshot snapshot;
+
+        public DebugExecutionEngine(TriggerType trigger, IScriptContainer container, Neo.Persistence.Snapshot snapshot, Neo.Fixed8 gas, bool testMode = false) 
+            : base(trigger, container, snapshot, gas, testMode)
+        {
+            this.snapshot = snapshot;
+        }
+
+        public static IExecutionEngine CreateExecutionEngine(NeoDebug.Models.Contract _, LaunchArguments __)
+        {
+            using (var snapshot = Neo.Ledger.Blockchain.Singleton.GetSnapshot())
+            {
+                return new DebugExecutionEngine(TriggerType.Application, null, snapshot, default, true);
+            }
+        }
+
+        VMState IExecutionEngine.State { get => State; set { State = value; } }
+
+        IEnumerable<StackItem> IExecutionEngine.ResultStack => ResultStack;
+
+        ExecutionContext IExecutionEngine.CurrentContext => CurrentContext;
+
+        RandomAccessStack<ExecutionContext> IExecutionEngine.InvocationStack => InvocationStack;
+
+        void IExecutionEngine.ExecuteNext() => ExecuteNext();
+
+        IVariableContainer IExecutionEngine.GetStorageContainer(IVariableContainerSession session)
+            => new StorageServiceContainer(session, snapshot, scriptHash);
+
+        void IExecutionEngine.LoadContract(NeoDebug.Models.Contract contract)
+        {
+            scriptHash = new Neo.UInt160(contract.ScriptHash);
+        }
+
+        ExecutionContext IExecutionEngine.LoadScript(byte[] script, int rvcount) => LoadScript(script, rvcount);
+    }
+}
--- src/neo-express/Node/NodeUtility.cs
+++ src/neo-express/Node/NodeUtility.cs
@@ -13,6 +13,7 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,7 +25,7 @@ namespace NeoExpress.Node
         {
             var tcs = new TaskCompletionSource<bool>();
 
-            ThreadPool.QueueUserWorkItem(_ =>
+            Task.Run(() =>
             {
                 try
                 {
@@ -36,7 +37,8 @@ namespace NeoExpress.Node
 
                         system.StartNode(node.TcpPort, node.WebSocketPort);
                         system.StartConsensus(wallet);
-                        system.StartRpc(IPAddress.Any, node.RpcPort, wallet);
+                        system.StartRpc(IPAddress.Loopback, node.RpcPort, wallet);
+                        StartDebug(new IPEndPoint(IPAddress.Loopback, node.DebugPort), writer, cancellationToken);
 
                         cancellationToken.WaitHandle.WaitOne();
                     }
@@ -56,6 +58,60 @@ namespace NeoExpress.Node
             });
 
             return tcs.Task;
+        }
+
+        public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            {
+                if (task != await Task.WhenAny(task, tcs.Task))
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+            }
+
+            return await task;
+        }
+
+        private static void StartDebug(IPEndPoint endPoint, TextWriter writer, CancellationToken cancellationToken)
+        {
+            Task.Run(async () =>
+            {
+                writer.WriteLine($"DEBUGGER listening on {endPoint}");
+                var listener = new TcpListener(endPoint);
+                listener.Start();
+
+                // TcpListener.AcceptSocketAsync doesn't support CancellationToken, so Stop the listener when 
+                // cancellationToken is triggered
+                using (cancellationToken.Register(() => listener.Stop()))
+                {
+                    while (true)
+                    {
+                        var clientSocket = await listener.AcceptSocketAsync().WithCancellation(cancellationToken);
+
+                        ThreadPool.QueueUserWorkItem(_ =>
+                        {
+                            using (clientSocket)
+                            using (var stream = new NetworkStream(clientSocket))
+                            {
+                                var adapter = new NeoDebug.DebugAdapter(stream, stream,
+                                    DebugExecutionEngine.CreateExecutionEngine,
+                                    Neo.Cryptography.Crypto.Default.Hash160,
+                                    (cat, msg) => writer.WriteLine($"{DateTimeOffset.Now.ToString("HH:mm:ss.ff")} DEBUGGER {cat} {msg}"));
+
+                                // DebugAdapter.Run doesn't support CancellationToken, so Stop the Protocol when 
+                                // cancellationToken is triggered
+                                using (cancellationToken.Register(() => adapter.Protocol.Stop()))
+                                {
+                                    adapter.Run();
+                                    adapter.Protocol.WaitForReader();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
         }
 
         private static bool CoinUnspent(Coin c)

6dd7a7e149ae018a48a108c7dcc428881c615747
Harry Pierson <harrypierson@outlook.com>
2019-11-04 13:46:01 -0800

    fix deploy command missing using statement

--- src/neo-express/Commands/ContractCommand.Deploy.cs
+++ src/neo-express/Commands/ContractCommand.Deploy.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace NeoExpress.Commands

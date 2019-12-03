a27f1c25c14bd07e9b52f34671231c9e10f1c628
Harry Pierson <harrypierson@hotmail.com>
2019-12-03 08:42:23 -0800

    show command user friendly output + aliases (#24)
    * aliases + descriptions for show subcommands
    
    * user friendly output for show commands

---
+++ src/neo2/Models/AccountResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Neo2Express.Models
+{
+    public class AccountResponse
+    {
+        [JsonProperty("version")]
+        public long Version { get; set; }
+
+        [JsonProperty("script_hash")]
+        public string ScriptHash { get; set; } = string.Empty;
+
+        [JsonProperty("frozen")]
+        public bool Frozen { get; set; }
+
+        [JsonProperty("balances")]
+        public AccountBalance[] Balances { get; set; } = Array.Empty<AccountBalance>();
+    }
+}
---
+++ src/neo2/Models/ClaimableTransaction.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Neo2Express.Models
+{
+    public class ClaimableTransaction
+    {
+        [JsonProperty("txid")]
+        public string TransactionId { get; set; } = string.Empty;
+
+        [JsonProperty("n")]
+        public long Index { get; set; }
+
+        [JsonProperty("value")]
+        public decimal Value { get; set; }
+
+        [JsonProperty("start_height")]
+        public long StartHeight { get; set; }
+
+        [JsonProperty("end_height")]
+        public long EndHeight { get; set; }
+
+        [JsonProperty("generated")]
+        public double Generated { get; set; }
+
+        [JsonProperty("sys_fee")]
+        public double SystemFee { get; set; }
+
+        [JsonProperty("unclaimed")]
+        public double Unclaimed { get; set; }
+    }
+}

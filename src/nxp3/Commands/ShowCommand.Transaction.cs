1ed37a011eb896f32402a21258242a98b30d513c
Harry Pierson <harrypierson@ngd.neo.org>
2020-04-28 08:18:55 -0700

    show transaction command

---
+++ src/nxp3/Commands/ShowCommand.Transaction.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using NeoExpress.Neo3;
+
+namespace nxp3.Commands
+{
+    partial class ShowCommand
+    {
+        [Command("transaction", "tx")]
+        class Transaction
+        {
+            [Argument(0)]
+            private string TransactionHash { get; } = string.Empty;
+
+            [Option]
+            private string Input { get; } = string.Empty;
+
+            private async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+            {
+                try
+                {
+                    var (chain, _) = Program.LoadExpressChain(Input);
+                    var blockchainOperations = new BlockchainOperations();
+
+                    var tx = await blockchainOperations.ShowTransaction(chain, TransactionHash);
+                    console.WriteLine(tx.ToJson().ToString(true));
+                    return 0;                
+                }
+                catch (Exception ex)
+                {
+                    console.WriteLine(ex.Message);
+                    app.ShowHelp();
+                    return 1;
+                }
+            }
+
+        }
+    }
+}
--- src/nxp3/Commands/ShowCommand.cs
+++ src/nxp3/Commands/ShowCommand.cs
@@ -3,7 +3,7 @@ using McMaster.Extensions.CommandLineUtils;
 namespace nxp3.Commands
 {
     [Command("show")]
-    [Subcommand(typeof(Balance))]
+    [Subcommand(typeof(Balance), typeof(Transaction))]
     partial class ShowCommand
     {
         private int OnExecute(CommandLineApplication app, IConsole console)

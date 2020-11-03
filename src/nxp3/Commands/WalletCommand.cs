624fd5964d7bb17f88baed8ef0eaa2794eae2528
Harry <harrypierson@ngd.neo.org>
2020-11-03 10:09:59 -0800

    wallet delete command

--- src/nxp3/Commands/WalletCommand.cs
+++ src/nxp3/Commands/WalletCommand.cs
@@ -3,7 +3,7 @@ using McMaster.Extensions.CommandLineUtils;
 namespace nxp3.Commands
 {
     [Command("wallet")]
-    [Subcommand(typeof(Create), typeof(Export), typeof(List))]
+    [Subcommand(typeof(Create), typeof(Delete), typeof(Export), typeof(List))]
     partial class WalletCommand
     {
         internal int OnExecute(CommandLineApplication app, IConsole console)

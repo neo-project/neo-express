3a0cdd934fe1841b9257ee57b2cc85dfbc3fbbd0
Harry Pierson <harrypierson@hotmail.com>
2021-03-23 11:57:00 -0700

    add --witness-scope option to contract invoke + deploy (#119)
    * add --witness-scope option to contract invoke + deploy
    
    * update release action version in release workflow
    
    * release workflow upload-artifact
    
    Co-authored-by: Harry <harrypierson@outlook.com>

--- src/neoxp/Commands/ContractCommand.Invoke.cs
+++ src/neoxp/Commands/ContractCommand.Invoke.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Neo.Network.P2P.Payloads;
 
 namespace NeoExpress.Commands
 {
@@ -27,11 +28,12 @@ namespace NeoExpress.Commands
             [Argument(1, Description = "Account to pay contract invocation GAS fee")]
             internal string Account { get; init; } = string.Empty;
 
+            [Option(Description = "Witness Scope to use for transaction signer (Default: CalledByEntry)")]
+            [AllowedValues(StringComparison.OrdinalIgnoreCase, "None", "CalledByEntry", "Global")]
+            internal WitnessScope WitnessScope { get; init; } = WitnessScope.CalledByEntry;
+
             [Option(Description = "Invoke contract for results (does not cost GAS)")]
             internal bool Results { get; init; } = false;
-
-            [Option(Description = "Path to neo-express data file")]
-            internal string Input { get; init; } = string.Empty;
 
             [Option("--gas|-g", CommandOptionType.SingleValue, Description = "Additional GAS to apply to the contract invocation")]
             internal decimal AdditionalGas { get; init; } = 0;
@@ -42,7 +44,10 @@ namespace NeoExpress.Commands
             [Option(Description = "Output as JSON")]
             internal bool Json { get; init; } = false;
 
-            internal static async Task ExecuteTxAsync(IExpressChainManager chainManager, IExpressNode expressNode, string invocationFile, string accountName, IFileSystem fileSystem, System.IO.TextWriter writer, bool json = false)
+            [Option(Description = "Path to neo-express data file")]
+            internal string Input { get; init; } = string.Empty;
+
+            internal static async Task ExecuteTxAsync(IExpressChainManager chainManager, IExpressNode expressNode, string invocationFile, string accountName, WitnessScope witnessScope, IFileSystem fileSystem, System.IO.TextWriter writer, bool json = false)
             {
                 if (!fileSystem.File.Exists(invocationFile))
                 {
@@ -56,7 +61,7 @@ namespace NeoExpress.Commands
 
                 var parser = await expressNode.GetContractParameterParserAsync(chainManager).ConfigureAwait(false);
                 var script = await parser.LoadInvocationScriptAsync(invocationFile).ConfigureAwait(false);
-                var txHash = await expressNode.ExecuteAsync(wallet, account.ScriptHash, script).ConfigureAwait(false);
+                var txHash = await expressNode.ExecuteAsync(wallet, account.ScriptHash, witnessScope, script).ConfigureAwait(false);
                 await writer.WriteTxHashAsync(txHash, "Deployment", json).ConfigureAwait(false);
             }
 
@@ -108,7 +113,7 @@ namespace NeoExpress.Commands
                     }
                     else
                     {
-                        await ExecuteTxAsync(chainManager, expressNode, InvocationFile, Account, fileSystem, console.Out, Json);
+                        await ExecuteTxAsync(chainManager, expressNode, InvocationFile, Account, WitnessScope, fileSystem, console.Out, Json);
                     }
 
                     return 0;

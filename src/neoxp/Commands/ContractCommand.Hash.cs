ec475069b3f238c2d5569d1520ada170e1b3a1e8
Harry Pierson <harrypierson@hotmail.com>
2021-08-12 09:54:38 -0700

    Add contract hash command (#170)
    * Add contract hash command - Fixes #169
    
    * add descriptions for contract list and storage commands
    
    * remove global.json
    
    Co-authored-by: Harry <harrypierson@outlook.com>

---
+++ src/neoxp/Commands/ContractCommand.Hash.cs
@@ -0,0 +1,61 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Abstractions;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using static Neo.SmartContract.Helper;
+
+namespace NeoExpress.Commands
+{
+    partial class ContractCommand
+    {
+        [Command(Name = "hash", Description = "Get contract hash for contract path and deployment account")]
+        internal class Hash
+        {
+            readonly IExpressChainManagerFactory chainManagerFactory;
+            readonly IFileSystem fileSystem;
+
+            public Hash(IExpressChainManagerFactory chainManagerFactory, IFileSystem fileSystem)
+            {
+                this.chainManagerFactory = chainManagerFactory;
+                this.fileSystem = fileSystem;
+            }
+
+            [Argument(0, Description = "Path to contract .nef file")]
+            [Required]
+            internal string Contract { get; init; } = string.Empty;
+
+            [Argument(1, Description = "Account that would deploy the contract")]
+            [Required]
+            internal string Account { get; init; } = string.Empty;
+
+            [Option(Description = "Path to neo-express data file")]
+            internal string Input { get; init; } = string.Empty;
+
+            internal async Task<int> OnExecuteAsync(IConsole console)
+            {
+                try
+                {
+                    var (chainManager, _) = chainManagerFactory.LoadChain(Input);
+                    if (!chainManager.Chain.TryGetAccountHash(Account, out var accountHash, chainManager.ProtocolSettings))
+                    {
+                        throw new Exception($"{Account} account not found.");
+                    }
+
+                    var (nefFile, manifest) = await fileSystem.LoadContractAsync(Contract).ConfigureAwait(false);
+                    var contractHash = GetContractHash(accountHash, nefFile.CheckSum, manifest.Name);
+
+                    await console.Out.WriteLineAsync($"{contractHash}").ConfigureAwait(false);
+
+                    return 0;
+                }
+                catch (Exception ex)
+                {
+                    await console.Error.WriteLineAsync(ex.Message);
+                    return 1;
+                }
+            }
+
+        }
+    }
+}

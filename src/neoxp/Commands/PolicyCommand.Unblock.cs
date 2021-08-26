7352a9c5034cf203a692b3efe7cbf467cab4b6f7
Harry <harrypierson@outlook.com>
2021-08-26 16:44:49 -0700

    fix unblock command

--- src/neoxp/Commands/PolicyCommand.Unblock.cs
+++ src/neoxp/Commands/PolicyCommand.Unblock.cs
@@ -46,7 +46,7 @@ namespace NeoExpress.Commands
                     var (chainManager, _) = chainManagerFactory.LoadChain(Input);
                     var password = chainManager.Chain.ResolvePassword(Account, Password);
                     using var txExec = txExecutorFactory.Create(chainManager, Trace, Json);
-                    await txExec.BlockAsync(ScriptHash, Account, Password).ConfigureAwait(false);
+                    await txExec.UnblockAsync(ScriptHash, Account, Password).ConfigureAwait(false);
                     return 0;
                 }
                 catch (Exception ex)

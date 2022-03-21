3ea29881d8be352cedaeebd8b8b16e49aee3aed6
Harry <harrypierson@outlook.com>
2022-03-21 11:52:22 -0700

    add --json option to wallet list

--- src/neoxp/Commands/WalletCommand.List.cs
+++ src/neoxp/Commands/WalletCommand.List.cs
@@ -5,6 +5,7 @@ using McMaster.Extensions.CommandLineUtils;
 using Neo.BlockchainToolkit.Models;
 using Neo.IO;
 using NeoExpress.Models;
+using Newtonsoft.Json;
 
 namespace NeoExpress.Commands
 {
@@ -23,43 +24,92 @@ namespace NeoExpress.Commands
             [Option(Description = "Path to neo-express data file")]
             internal string Input { get; init; } = string.Empty;
 
-            internal void Execute(TextWriter writer)
-            {
-                var (chainManager, _) = chainManagerFactory.LoadChain(Input);
-                var chain = chainManager.Chain;
-
-                for (int i = 0; i < chain.ConsensusNodes.Count; i++)
-                {
-                    PrintWalletInfo(chain.ConsensusNodes[i].Wallet);
-                }
-
-                for (int i = 0; i < chain.Wallets.Count; i++)
-                {
-                    PrintWalletInfo(chain.Wallets[i]);
-                }
-
-                void PrintWalletInfo(ExpressWallet wallet)
-                {
-                    writer.WriteLine(wallet.Name);
-
-                    foreach (var account in wallet.Accounts)
-                    {
-                        var devAccount = DevWalletAccount.FromExpressWalletAccount(chainManager.ProtocolSettings, account);
-                        var key = devAccount.GetKey() ?? throw new Exception();
-
-                        writer.WriteLine($"  {account.ScriptHash} ({(account.IsDefault ? "Default" : account.Label)})");
-                        writer.WriteLine($"    address bytes: {BitConverter.ToString(devAccount.ScriptHash.ToArray())}");
-                        writer.WriteLine($"    public key:    {key.PublicKey.EncodePoint(true).ToHexString()}");
-                        writer.WriteLine($"    private key:   {key.PrivateKey.ToHexString()}");
-                    }
-                }
-            }
+            [Option(Description = "Output as JSON")]
+            internal bool Json { get; init; } = false;
 
             internal int OnExecute(CommandLineApplication app, IConsole console)
             {
                 try
                 {
-                    Execute(console.Out);
+                    var (chainManager, _) = chainManagerFactory.LoadChain(Input);
+                    var chain = chainManager.Chain;
+
+                    if (Json)
+                    {
+                        using var writer = new JsonTextWriter(console.Out) { Formatting = Formatting.Indented };
+                        writer.WriteStartArray();
+
+                        foreach (var node in chain.ConsensusNodes)
+                        {
+                            PrintWalletInfo(node.Wallet);
+                        }
+
+                        foreach (var wallet in chain.Wallets)
+                        {
+                            PrintWalletInfo(wallet);
+                        }
+
+                        writer.WriteEndArray();
+
+                        void PrintWalletInfo(ExpressWallet wallet)
+                        {
+                            writer.WriteStartObject();
+                            writer.WritePropertyName("name");
+                            writer.WriteValue(wallet.Name);
+                            writer.WritePropertyName("accounts");
+                            writer.WriteStartArray();
+                            foreach (var account in wallet.Accounts)
+                            {
+                                var devAccount = DevWalletAccount.FromExpressWalletAccount(chainManager.ProtocolSettings, account);
+                                var keyPair = devAccount.GetKey() ?? throw new Exception();
+
+                                writer.WriteStartObject();
+                                writer.WritePropertyName("name");
+                                writer.WriteValue(account.IsDefault ? "Default" : account.Label);
+                                writer.WritePropertyName("address");
+                                writer.WriteValue(devAccount.Address);
+                                writer.WritePropertyName("script-hash");
+                                writer.WriteValue(devAccount.ScriptHash.ToString());
+                                writer.WritePropertyName("private-key");
+                                writer.WriteValue(keyPair.PrivateKey.ToHexString());
+                                writer.WritePropertyName("public-key");
+                                writer.WriteValue(keyPair.PublicKey.EncodePoint(true).ToHexString());
+                                writer.WriteEndObject();
+                            }
+                            writer.WriteEndArray();
+                            writer.WriteEndObject();
+                        }
+                    }
+                    else
+                    {
+                        var writer = console.Out;
+                        foreach (var node in chain.ConsensusNodes)
+                        {
+                            PrintWalletInfo(node.Wallet);
+                        }
+
+                        foreach (var wallet in chain.Wallets)
+                        {
+                            PrintWalletInfo(wallet);
+                        }
+
+                        void PrintWalletInfo(ExpressWallet wallet)
+                        {
+                            writer.WriteLine(wallet.Name);
+
+                            foreach (var account in wallet.Accounts)
+                            {
+                                var devAccount = DevWalletAccount.FromExpressWalletAccount(chainManager.ProtocolSettings, account);
+                                var keyPair = devAccount.GetKey() ?? throw new Exception();
+
+                                writer.WriteLine($"  {devAccount.Address} ({(account.IsDefault ? "Default" : account.Label)})");
+                                writer.WriteLine($"    script hash: {BitConverter.ToString(devAccount.ScriptHash.ToArray())}");
+                                writer.WriteLine($"    public key:    {keyPair.PublicKey.EncodePoint(true).ToHexString()}");
+                                writer.WriteLine($"    private key:   {keyPair.PrivateKey.ToHexString()}");
+                            }
+                        }
+                    }
+
                     return 0;
                 }
                 catch (Exception ex)
@@ -67,6 +117,7 @@ namespace NeoExpress.Commands
                     app.WriteException(ex);
                     return 1;
                 }
+
             }
         }
     }

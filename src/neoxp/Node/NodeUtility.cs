0d14b4395e69f59f2e8678bfba5b9119ccd1bbb1
Harry <harrypierson@outlook.com>
2022-04-28 13:36:36 -0700

    Move EventWrapper into NodeUtility

--- src/neoxp/Node/NodeUtility.cs
+++ src/neoxp/Node/NodeUtility.cs
@@ -585,6 +585,36 @@ namespace NeoExpress.Node
             }
         }
 
+        // EventWrapper class receives events from the Akka EventStream and invokes the provided callback.
+        // Code taken from neo-gui project:
+        //   https://github.com/neo-project/neo-node/blob/master/neo-gui/IO/Actors/EventWrapper.cs
+        class EventWrapper<T> : Akka.Actor.UntypedActor
+        {
+            readonly Action<T> callback;
+
+            public EventWrapper(Action<T> callback)
+            {
+                this.callback = callback;
+                Context.System.EventStream.Subscribe(Self, typeof(T));
+            }
+
+            protected override void OnReceive(object message)
+            {
+                if (message is T obj) callback(obj);
+            }
+
+            protected override void PostStop()
+            {
+                Context.System.EventStream.Unsubscribe(Self);
+                base.PostStop();
+            }
+
+            public static Akka.Actor.Props Props(Action<T> callback)
+            {
+                return Akka.Actor.Props.Create(() => new EventWrapper<T>(callback));
+            }
+        }
+
         // Need an IVerifiable.GetScriptHashesForVerifying implementation that doesn't
         // depend on the DataCache snapshot parameter in order to create a 
         // ContractParametersContext without direct access to node data.

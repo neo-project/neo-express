0b08a6cb113f51a4220d73c062ac10746e4a20d2
Harry <harrypierson@outlook.com>
2022-04-29 10:32:10 -0700

    rework reset command

--- src/neoxp/Commands/ResetCommand.cs
+++ src/neoxp/Commands/ResetCommand.cs
@@ -7,12 +7,17 @@ namespace NeoExpress.Commands
     [Command("reset", Description = "Reset neo-express instance node")]
     class ResetCommand
     {
-        readonly IFileSystem fileSystem;
+        readonly IExpressFile expressFile;
 
-        public ResetCommand(IFileSystem fileSystem)
+        public ResetCommand(IExpressFile expressFile)
         {
-            this.fileSystem = fileSystem;
+            this.expressFile = expressFile;
         }
+
+        public ResetCommand(CommandLineApplication app) : this(app.GetExpressFile())
+        {
+        }
+
 
         [Argument(0, Description = "Index of node to reset")]
         internal int? NodeIndex { get; }
@@ -26,14 +31,16 @@ namespace NeoExpress.Commands
         [Option(Description = "Reset all nodes")]
         internal bool All { get; }
 
-        internal void Execute(IConsole console)
+        internal int OnExecute(CommandLineApplication app) => app.Execute(this.Execute);
+
+        internal void Execute(IFileSystem fileSystem, IConsole console)
         {
             if (NodeIndex.HasValue && All)
             {
                 throw new InvalidOperationException("Only one of NodeIndex or --all can be specified");
             }
 
-            var (chain, _) = fileSystem.LoadExpressChain(Input);
+            var chain = expressFile.Chain;
 
             if (All)
             {
@@ -53,20 +60,6 @@ namespace NeoExpress.Commands
 
                 fileSystem.ResetNode(chain.ConsensusNodes[nodeIndex], Force);
                 console.Out.WriteLine($"node {nodeIndex} reset");
-            }
-        }
-
-        internal int OnExecute(CommandLineApplication app, IConsole console)
-        {
-            try
-            {
-                Execute(console);
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                app.WriteException(ex);
-                return 1;
             }
         }
     }

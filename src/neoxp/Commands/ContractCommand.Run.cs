ed12b4e945552b0dcf18ee88c2b30f1df89e281d
Harry <harrypierson@outlook.com>
2022-05-06 17:06:16 -0700

    Initial RUN support

--- src/neoxp/Commands/ContractCommand.Run.cs
+++ src/neoxp/Commands/ContractCommand.Run.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO.Abstractions;
+using System.Numerics;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Neo;
+using Neo.BlockchainToolkit;
 using Neo.Network.P2P.Payloads;
+using Neo.SmartContract;
+using Neo.VM;
 
 namespace NeoExpress.Commands
 {
@@ -12,11 +18,15 @@ namespace NeoExpress.Commands
         [Command(Name = "run", Description = "Invoke a contract using parameters passed on command line")]
         internal class Run
         {
-            readonly IFileSystem fileSystem;
+            readonly IExpressFile expressFile;
 
-            public Run(IFileSystem fileSystem)
+            public Run(IExpressFile expressFile)
             {
-                this.fileSystem = fileSystem;
+                this.expressFile = expressFile;
+            }
+
+            public Run(CommandLineApplication app) : this(app.GetExpressFile())
+            {
             }
 
             [Argument(0, Description = "Contract name or invocation hash")]
@@ -52,40 +62,110 @@ namespace NeoExpress.Commands
             [Option(Description = "Output as JSON")]
             internal bool Json { get; init; } = false;
 
-            
-            internal string Input { get; init; } = string.Empty;
+            internal Task<int> OnExecuteAsync(CommandLineApplication app)
+                => app.ExecuteAsync(this.ExecuteAsync);
 
-            internal async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+            internal async Task ExecuteAsync(IConsole console)
             {
-                try
+                using var expressNode = expressFile.GetExpressNode(Trace);
+                var script = await BuildInvocationScriptAsync(expressNode, Contract, Method, Arguments).ConfigureAwait(false);
+
+                if (Results)
                 {
-                    if (string.IsNullOrEmpty(Account) && !Results)
-                    {
-                        throw new Exception("Either --account or --results must be specified");
-                    }
-
-                    var (chain, _) = fileSystem.LoadExpressChain(Input);
-                    using var txExec = new TransactionExecutor(fileSystem, chain, Trace, Json, console.Out); 
-                    var script = await txExec.BuildInvocationScriptAsync(Contract, Method, Arguments).ConfigureAwait(false);
-
-                    if (Results)
-                    {
-                        await txExec.InvokeForResultsAsync(script, Account, WitnessScope);
-                    }
-                    else
-                    {
-                        var password = chain.ResolvePassword(Account, Password);
-                        await txExec.ContractInvokeAsync(script, Account, password, WitnessScope, AdditionalGas);
-                    }
-
-                    return 0;
+                    var signer = expressNode.Chain.TryResolveAccountHash(Account, out var hash)
+                        ? new Signer { Account = hash, Scopes = WitnessScope }
+                        : null;
+                    var result = await expressNode.InvokeAsync(script, signer).ConfigureAwait(false);
+                    await console.Out.WriteLineAsync(result.ToJson().ToString(true)).ConfigureAwait(false);
                 }
-                catch (Exception ex)
+                else
                 {
-                    app.WriteException(ex, showInnerExceptions: true);
-                    return 1;
+                    var password = expressFile.ResolvePassword(Account, Password);
+                    var (wallet, accountHash) = expressNode.ExpressFile.ResolveSigner(Account, password);
+
+                    var txHash = await expressNode.ExecuteAsync(wallet, accountHash, WitnessScope, script).ConfigureAwait(false);
+                    await console.Out.WriteTxHashAsync(txHash, "Invocation", Json).ConfigureAwait(false);
                 }
             }
+
+            public static async Task<Script> BuildInvocationScriptAsync(IExpressNode expressNode, string contract, string operation, IReadOnlyList<string>? arguments = null)
+            {
+                if (string.IsNullOrEmpty(operation))
+                    throw new InvalidOperationException($"invalid contract operation \"{operation}\"");
+
+                var parser = await expressNode.GetContractParameterParserAsync().ConfigureAwait(false);
+                var scriptHash = parser.TryLoadScriptHash(contract, out var value)
+                    ? value
+                    : UInt160.TryParse(contract, out var uint160)
+                        ? uint160
+                        : throw new InvalidOperationException($"contract \"{contract}\" not found");
+
+                arguments ??= Array.Empty<string>();
+                var @params = new ContractParameter[arguments.Count];
+                for (int i = 0; i < arguments.Count; i++)
+                {
+                    @params[i] = ConvertArg(arguments[i], parser);
+                }
+
+                using var scriptBuilder = new ScriptBuilder();
+                scriptBuilder.EmitDynamicCall(scriptHash, operation, @params);
+                return scriptBuilder.ToArray();
+
+                static ContractParameter ConvertArg(string arg, ContractParameterParser parser)
+                {
+                    if (bool.TryParse(arg, out var boolArg))
+                    {
+                        return new ContractParameter()
+                        {
+                            Type = ContractParameterType.Boolean,
+                            Value = boolArg
+                        };
+                    }
+
+                    if (long.TryParse(arg, out var longArg))
+                    {
+                        return new ContractParameter()
+                        {
+                            Type = ContractParameterType.Integer,
+                            Value = new BigInteger(longArg)
+                        };
+                    }
+
+                    return parser.ParseParameter(arg);
+                }
+            }
+
+            // internal async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+            // {
+            //     try
+            //     {
+            //         if (string.IsNullOrEmpty(Account) && !Results)
+            //         {
+            //             throw new Exception("Either --account or --results must be specified");
+            //         }
+
+            //         var (chain, _) = fileSystem.LoadExpressChain(Input);
+            //         using var txExec = new TransactionExecutor(fileSystem, chain, Trace, Json, console.Out); 
+            //         var script = await txExec.BuildInvocationScriptAsync(Contract, Method, Arguments).ConfigureAwait(false);
+
+            //         if (Results)
+            //         {
+            //             await txExec.InvokeForResultsAsync(script, Account, WitnessScope);
+            //         }
+            //         else
+            //         {
+            //             var password = chain.ResolvePassword(Account, Password);
+            //             await txExec.ContractInvokeAsync(script, Account, password, WitnessScope, AdditionalGas);
+            //         }
+
+            //         return 0;
+            //     }
+            //     catch (Exception ex)
+            //     {
+            //         app.WriteException(ex, showInnerExceptions: true);
+            //         return 1;
+            //     }
+            // }
         }
     }
 }

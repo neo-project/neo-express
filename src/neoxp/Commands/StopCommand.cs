f6d98ff14f080f625231b0841fae487fc3fa926b
Harry <harrypierson@outlook.com>
2022-06-13 11:05:22 -0700

    stop cmd

--- src/neoxp/Commands/StopCommand.cs
+++ src/neoxp/Commands/StopCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
+using Neo.BlockchainToolkit.Models;
 
 namespace NeoExpress.Commands
 {
@@ -25,52 +26,49 @@ namespace NeoExpress.Commands
         [Option(Description = "Stop all nodes")]
         internal bool All { get; }
 
-        // internal async Task ExecuteAsync(IConsole console)
-        // {
-        //     if (NodeIndex.HasValue && All)
-        //     {
-        //         throw new InvalidOperationException("Only one of NodeIndex or --all can be specified");
-        //     }
+        internal Task<int> OnExecuteAsync(CommandLineApplication app) => app.ExecuteAsync(this.ExecuteAsync);
 
-        //     var (chainManager, _) = chainManagerFactory.LoadChain(Input);
-        //     var chain = chainManager.Chain;
-
-        //     if (All)
-        //     {
-        //         var tasks = new Task[chain.ConsensusNodes.Count];
-        //         for (int i = 0; i < chain.ConsensusNodes.Count; i++)
-        //         {
-        //             tasks[i] = chainManager.StopNodeAsync(chain.ConsensusNodes[i]);
-        //         }
-        //         await Task.WhenAll(tasks).ConfigureAwait(false);
-        //         await console.Out.WriteLineAsync($"all nodes stopped").ConfigureAwait(false);
-        //     }
-        //     else
-        //     {
-        //         var nodeIndex = NodeIndex.HasValue
-        //             ? NodeIndex.Value
-        //             : chain.ConsensusNodes.Count == 1
-        //                 ? 0
-        //                 : throw new InvalidOperationException("node index or --all must be specified when resetting a multi-node chain");
-
-        //         var wasRunning = await chainManager.StopNodeAsync(chain.ConsensusNodes[nodeIndex]).ConfigureAwait(false);
-        //         await console.Out.WriteLineAsync($"node {nodeIndex} {(wasRunning ? "stopped" : "was not running")}").ConfigureAwait(false);
-        //     }
-        // }
-
-        internal async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+        internal async Task ExecuteAsync(IConsole console)
         {
-            try
+            if (NodeIndex.HasValue && All)
             {
-                // await ExecuteAsync(console).ConfigureAwait(false);
-                return 0;
+                throw new InvalidOperationException("Only one of NodeIndex or --all can be specified");
             }
-            catch (Exception ex)
+
+            if (All)
             {
-                app.WriteException(ex);
-                return 1;
+                var tasks = new Task[chain.ConsensusNodes.Count];
+                for (int i = 0; i < chain.ConsensusNodes.Count; i++)
+                {
+                    tasks[i] = StopNodeAsync(chain.ConsensusNodes[i]);
+                }
+                await Task.WhenAll(tasks).ConfigureAwait(false);
+                await console.Out.WriteLineAsync($"all nodes stopped").ConfigureAwait(false);
+            }
+            else
+            {
+                var nodeIndex = NodeIndex.HasValue
+                    ? NodeIndex.Value
+                    : chain.ConsensusNodes.Count == 1
+                        ? 0
+                        : throw new ArgumentException("node index or --all must be specified when resetting a multi-node chain");
+                if (nodeIndex < 0 || nodeIndex >= chain.ConsensusNodes.Count) { throw new ArgumentException("Invalid node index"); }
+
+                var wasRunning = await StopNodeAsync(chain.ConsensusNodes[nodeIndex]).ConfigureAwait(false);
+                await console.Out.WriteLineAsync($"node {nodeIndex} {(wasRunning ? "stopped" : "was not running")}").ConfigureAwait(false);
+            }
+
+            static async Task<bool> StopNodeAsync(ExpressConsensusNode node)
+            {
+                if (!node.IsRunning()) return false;
+
+                var rpcClient = new Neo.Network.RPC.RpcClient(new Uri($"http://localhost:{node.RpcPort}"));
+                var json = await rpcClient.RpcSendAsync("expressshutdown").ConfigureAwait(false);
+                var processId = int.Parse(json["process-id"].AsString());
+                var process = System.Diagnostics.Process.GetProcessById(processId);
+                await process.WaitForExitAsync().ConfigureAwait(false);
+                return true;
             }
         }
-
     }
 }

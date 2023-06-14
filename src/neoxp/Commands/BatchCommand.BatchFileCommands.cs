865194209e335979f19e924f0f96eedf2326b25f
Harry <harrypierson@outlook.com>
2023-06-14 12:42:12 -0700

    Policy sync issue in batch command Fixes #281

--- src/neoxp/Commands/BatchCommand.BatchFileCommands.cs
+++ src/neoxp/Commands/BatchCommand.BatchFileCommands.cs
@@ -188,7 +188,7 @@ namespace NeoExpress.Commands
             }
 
             [Command("policy")]
-            [Subcommand(typeof(Block), typeof(Set), typeof(Unblock))]
+            [Subcommand(typeof(Block), typeof(Set), typeof(Sync), typeof(Unblock))]
             internal class Policy
             {
                 [Command("block")]

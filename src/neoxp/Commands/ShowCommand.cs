e65d7c4a028fead2153dadccdd5b10b249e60362
陈志同 <bitcoin2077@outlook.com>
2023-11-09 16:59:18 +0800

    Add method: transfernft & show nft (#304)
    * temp
    
    * TransferNFT
    
    * update
    
    * hex string
    
    * update
    
    * format
    
    * format
    
    * show nft
    
    * format
    
    ---------
    
    Co-authored-by: Jimmy <jinghui@wayne.edu>

--- src/neoxp/Commands/ShowCommand.cs
+++ src/neoxp/Commands/ShowCommand.cs
@@ -13,7 +13,7 @@ using McMaster.Extensions.CommandLineUtils;
 namespace NeoExpress.Commands
 {
     [Command("show", Description = "Show information")]
-    [Subcommand(typeof(Balance), typeof(Balances), typeof(Block), typeof(Notifications), typeof(Transaction))]
+    [Subcommand(typeof(Balance), typeof(Balances), typeof(Block), typeof(Notifications), typeof(Transaction), typeof(NFT))]
     partial class ShowCommand
     {
         internal int OnExecute(CommandLineApplication app, IConsole console)

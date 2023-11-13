8c1c53462d2111f1a9f2f7842e82fa2dc2467693
Christopher R Schuchardt <cschuchardt88@gmail.com>
2023-11-13 09:11:50 -0500

    dotnet format

--- src/neoxp/Validators/TokenBase.cs
+++ src/neoxp/Validators/TokenBase.cs
@@ -1,12 +1,14 @@
 // Copyright (C) 2015-2023 The Neo Project.
 //
-// The neo is free software distributed under the MIT software license,
-// see the accompanying file LICENSE in the main directory of the
-// project or http://www.opensource.org/licenses/mit-license.php
+// TokenBase.cs file belongs to neo-express project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
 // for more details.
 //
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
+
 using Neo;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;

6dff7b677ab4593dfd4183a14d4d864f3a798338
Christopher Schuchardt <cschuchardt88@gmail.com>
2023-11-13 10:09:47 -0500

    neoxp help suggests help (#321)
    * Fixed help text when just typing help with -- characters
    
    * Removed error command suggestions
    
    ---------
    
    Co-authored-by: Jimmy <jinghui@wayne.edu>

--- src/neoxp/Program.cs
+++ src/neoxp/Program.cs
@@ -61,14 +61,6 @@ namespace NeoExpress
             catch (CommandParsingException ex)
             {
                 await Console.Error.WriteLineAsync($"\x1b[1m\x1b[31m\x1b[40m{ex.Message}\x1b[0m");
-
-                if (ex is UnrecognizedCommandParsingException uex && uex.NearestMatches.Any())
-                {
-                    await Console.Error.WriteLineAsync();
-                    await Console.Error.WriteLineAsync("Did you mean this?");
-                    await Console.Error.WriteLineAsync("    " + uex.NearestMatches.First());
-                }
-
                 return 1;
             }
             catch (Exception ex)

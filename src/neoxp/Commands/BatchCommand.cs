a548511e867540b8fdfc29805b771efbbcef1310
Christopher R Schuchardt <cschuchardt88@gmail.com>
2023-11-13 06:59:58 -0500

    Add filenames to the file header of each *.cs file updated .editorconfig

--- src/neoxp/Commands/BatchCommand.cs
+++ src/neoxp/Commands/BatchCommand.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2023 The Neo Project.
 //
-// The neo is free software distributed under the MIT software license,
+// BatchCommand.cs is free software distributed under the MIT software license,
 // see the accompanying file LICENSE in the main directory of the
 // project or http://www.opensource.org/licenses/mit-license.php
 // for more details.
@@ -9,7 +9,6 @@
 // modifications are permitted.
 
 using McMaster.Extensions.CommandLineUtils;
-using Neo.BlockchainToolkit;
 using System.ComponentModel.DataAnnotations;
 using System.IO.Abstractions;
 
--- src/worknet/Commands/PrefetchCommand.cs
+++ src/worknet/Commands/PrefetchCommand.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2023 The Neo Project.
 //
-// The neo is free software distributed under the MIT software license,
+// PrefetchCommand.cs is free software distributed under the MIT software license,
 // see the accompanying file LICENSE in the main directory of the
 // project or http://www.opensource.org/licenses/mit-license.php
 // for more details.

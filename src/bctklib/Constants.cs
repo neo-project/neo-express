4d66c6512d69bc0d0a086fda15e6e05ad29a2b01
Christopher R Schuchardt <cschuchardt88@gmail.com>
2023-11-18 07:12:11 -0500

    Removed Init default for batch filename, is unused

--- src/bctklib/Constants.cs
+++ src/bctklib/Constants.cs
@@ -20,7 +20,6 @@ namespace Neo.BlockchainToolkit
 
         public const string EXPRESS_BATCH_EXTENSION = ".batch";
         public const string DEFAULT_BATCH_FILENAME = "default" + EXPRESS_BATCH_EXTENSION;
-        public const string DEFAULT_INIT_BATCH_FILENAME = "init" + EXPRESS_BATCH_EXTENSION;
         public const string DEFAULT_SETUP_BATCH_FILENAME = "setup" + EXPRESS_BATCH_EXTENSION;
 
         public const string DEAULT_POLICY_FILENAME = "default-policy" + JSON_EXTENSION;

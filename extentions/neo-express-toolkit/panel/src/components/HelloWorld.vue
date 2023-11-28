a85f3ada02530a6cae1ef4f06854dedc086f1905
Christopher R Schuchardt <cschuchardt88@gmail.com>
2023-11-28 02:35:07 -0500

    uses cache now

---
+++ extentions/neo-express-toolkit/panel/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+function sayHello(event)
+{
+  vscode.postMessage({
+    message: 'extension can listen to vue events by using postMessage method!'
+  })
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  
+  <button @click="sayHello">Say Hello</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ extentions/neo-express-toolkit/panel/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    }
+  },
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+})

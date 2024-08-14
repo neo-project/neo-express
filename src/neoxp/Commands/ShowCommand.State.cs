7ed60db891a2264b6f195cf05aa8de74fe3ccf3f
陈志同 <bitcoin2077@outlook.com>
2024-08-14 16:20:29 +0800

    Update ShowCommand.State.cs

--- src/neoxp/Commands/ShowCommand.State.cs
+++ src/neoxp/Commands/ShowCommand.State.cs
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2024 The Neo Project.
 //
-// ShowCommand.Block.cs file belongs to neo-express project and is free
+// ShowCommand.State.cs file belongs to neo-express project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
 // repository or http://www.opensource.org/licenses/mit-license.php

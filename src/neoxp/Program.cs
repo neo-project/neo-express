114dbc451539a9c7bc83b342531ba9d5efc3142e
Alvaro <amjarag@gmail.com>
2025-07-29 22:51:41 +0200

    Update header link

--- src/neoxp/Program.cs
+++ src/neoxp/Program.cs
@@ -3,7 +3,7 @@
 // Program.cs file belongs to neo-express project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
-// repository or http://www.opensource.org/licenses/mit-license.php
+// repository or https://opensource.org/license/MIT
 // for more details.
 //
 // Redistribution and use in source and binary forms with or without

a772bbebaa3e3455dc9f54d2a6c4af8339efa9f0
Fernando Diaz Toledano <shargon@gmail.com>
2025-07-30 09:52:17 +0200

    Apply new header

--- src/bctklib/formatters/UInt256Formatter.cs
+++ src/bctklib/formatters/UInt256Formatter.cs
@@ -3,8 +3,7 @@
 // UInt256Formatter.cs file belongs to neo-express project and is free
 // software distributed under the MIT software license, see the
 // accompanying file LICENSE in the main directory of the
-// repository or https://opensource.org/license/MIT
-// for more details.
+// repository or https://opensource.org/license/MIT for more details.
 //
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.

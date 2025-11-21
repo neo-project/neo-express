2ed32c17e0105a274f106a587ff61ea5f7e5bd4e
jimmy <jimmy@r3e.network>
2025-11-21 08:25:58 +0800

    Add license header for internals visibility

--- src/neoxp/Properties/AssemblyInfo.cs
+++ src/neoxp/Properties/AssemblyInfo.cs
@@ -1,3 +1,13 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// AssemblyInfo.cs file belongs to neo-express project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or https://opensource.org/license/MIT for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("test.workflowvalidation")]

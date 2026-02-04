e8c9d07b0c17154d4658403ee02a89bc0c5ef3df
Alvaro <amjarag@gmail.com>
2026-02-04 18:02:52 +0100

    Revert "update year (#520)"
    This reverts commit 658069feef2c4538050bc0da4a2acfda0eac166e.

--- src/neoxp/IExpressNode.cs
+++ src/neoxp/IExpressNode.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2015-2026 The Neo Project.
+// Copyright (C) 2015-2025 The Neo Project.
 //
 // IExpressNode.cs file belongs to neo-express project and is free
 // software distributed under the MIT software license, see the

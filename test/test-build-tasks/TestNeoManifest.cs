e37235c056e328dbe7236e322b20c2b542ad8498
Alvaro <amjarag@gmail.com>
2026-02-04 01:52:07 +0100

    update year

--- test/test-build-tasks/TestNeoManifest.cs
+++ test/test-build-tasks/TestNeoManifest.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2015-2025 The Neo Project.
+// Copyright (C) 2015-2026 The Neo Project.
 //
 // TestNeoManifest.cs file belongs to neo-express project and is free
 // software distributed under the MIT software license, see the

2ac097fa2adbe47e1df7f90acb32cc864c0f5ef8
Jimmy <jimmy@r3e.network>
2026-09-20 07:36:37 +0800

    test: format trace stream regression test

---
+++ test/test.bctklib/TraceDebugStreamTests.cs
@@ -0,0 +1,35 @@
+// Copyright (C) 2015-2026 The Neo Project.
+//
+// TraceDebugStreamTests.cs file belongs to neo-express project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or https://opensource.org/license/MIT for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Neo.BlockchainToolkit.TraceDebug;
+using System.IO;
+using Xunit;
+
+namespace test.bctklib;
+
+public class TraceDebugStreamTests
+{
+    [Fact]
+    public void FlushFailureIsExposedAfterDispose()
+    {
+        using var stream = new FlushFailingStream();
+        var sink = new TraceDebugStream(stream);
+
+        sink.ProtocolSettings(894710606, 53);
+        sink.Dispose();
+
+        Assert.IsType<IOException>(sink.WriteError);
+    }
+
+    private sealed class FlushFailingStream : MemoryStream
+    {
+        public override void Flush() => throw new IOException("flush failed");
+    }
+}
